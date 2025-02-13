dfcb6af7ecd4784b97e517ff8a9b64dbbf98ae85
Amir Haroun <amir.haroun@icloud.com>
2025-02-13 10:48:50 +0200

    changing ports

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ redisClient.connect().then(() => {
     });
 
     // Start the server
-    const PORT = 3000;
+    const PORT = 5492;
     server.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
